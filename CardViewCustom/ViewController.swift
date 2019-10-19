635ba04678810a2abd8d350f9dbf304f81ff5974
Alejandro Ariza <diego.ariza2@gmail.com>
2019-10-19 18:03:11 -0500

    🛠 Estructura inicial del proyecto.
     On branch master
     Your branch is up to date with 'origin/master'.
    
     Changes to be committed:
    	new file:   CardViewCustom.xcodeproj/project.pbxproj
    	new file:   CardViewCustom.xcodeproj/project.xcworkspace/contents.xcworkspacedata
    	new file:   CardViewCustom.xcodeproj/project.xcworkspace/xcshareddata/IDEWorkspaceChecks.plist
    	new file:   CardViewCustom.xcodeproj/project.xcworkspace/xcuserdata/diego.ariza.xcuserdatad/UserInterfaceState.xcuserstate
    	new file:   CardViewCustom.xcodeproj/xcuserdata/diego.ariza.xcuserdatad/xcschemes/xcschememanagement.plist
    	new file:   CardViewCustom/AppDelegate.swift
    	new file:   CardViewCustom/Assets.xcassets/AppIcon.appiconset/Contents.json
    	new file:   CardViewCustom/Assets.xcassets/Contents.json
    	new file:   CardViewCustom/Base.lproj/LaunchScreen.storyboard
    	new file:   CardViewCustom/Base.lproj/Main.storyboard
    	new file:   CardViewCustom/Info.plist
    	new file:   CardViewCustom/SceneDelegate.swift
    	new file:   CardViewCustom/ViewController.swift

---
+++ CardViewCustom/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CardViewCustom
+//
+//  Created by Alejandro Ariza on 10/19/19.
+//  Copyright © 2019 Alejandro Ariza. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
