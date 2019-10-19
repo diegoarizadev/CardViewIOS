4af1d5dc89f51632ad110e283aa445385e9098f0
Alejandro Ariza <diego.ariza2@gmail.com>
2019-10-19 18:46:05 -0500

    📍Se agrega viewController a la tarjeta custom  On branch master  Your branch is up to date with 'origin/master'.
     Changes to be committed:
    	modified:   CardViewCustom.xcodeproj/project.pbxproj
    	modified:   CardViewCustom.xcodeproj/project.xcworkspace/xcuserdata/diego.ariza.xcuserdatad/UserInterfaceState.xcuserstate
    	new file:   CardViewCustom/CardViewController.swift
    	modified:   CardViewCustom/CardViewController.xib

---
+++ CardViewCustom/CardViewController.swift
@@ -0,0 +1,14 @@
+//
+//  CardViewController.swift
+//  CardViewCustom
+//
+//  Created by Alejandro Ariza on 10/19/19.
+//  Copyright © 2019 Alejandro Ariza. All rights reserved.
+//
+
+import UIKit
+
+class  CardViewController: UIViewController {
+    
+    @IBOutlet weak var headerArea: UIView!
+}
